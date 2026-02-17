4bbdf90d9165f3b7ca37d207df099297212f0a2c
YibielS <yibielsantiago@hotmail.com>
2026-02-17 18:28:37 -0500

    Update Week5.js

--- Week5.js
+++ Week5.js
@@ -27,3 +27,52 @@ function showOddNumbers() {
   }
   document.getElementById("oddNumberResult").textContent = result;
 }
+
+function addMultiplesToArray() {
+  const arr = [];
+  const counter = document.getElementById("counter").textContent;
+  for (let i =5; i<= counter; i+=5){
+    arr.push(i);
+  }
+  arr.reverse();
+  console.log(arr);
+}
+
+function printCarObject() {
+  let car;
+  car.cType = document.getElementById("carType").value;
+  car.cMPG = document.getElementById("carMPG").value;
+  car.cColor = document.getElementById("carColor").value;
+  console.log(car);
+}
+
+function loadCar(int) {
+  let car;
+  if (num == 1) {
+    car = carObject1;
+  }
+else if (num == 2) {
+    car = carObject2;
+  }
+  else if (num == 3) {
+    car = carObject3;
+  }
+  document.getElementById("carType").value = car.cType;
+  document.getElementById("carMPG").value = car.cMPG;
+  document.getElementById("carColor").value = car.cColor;
+}
+
+function changeColor(int) {
+  const p = document.getElementById("styleParagraph");
+   if (num == 1) {
+    p.style.color = "red";
+  }
+else if (num == 2) {
+    p.style.color = "green";
+  }
+  else if (num == 3) {
+    p.style.color = "blue";
+  }
+
+
+  
