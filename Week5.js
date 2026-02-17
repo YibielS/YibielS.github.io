ac9d8a551c1c5bed1e1f27af62ab2183798bab5c
YibielS <yibielsantiago@hotmail.com>
2026-02-17 17:31:29 -0500

    Update Week5.js

--- Week5.js
+++ Week5.js
@@ -1,1 +1,29 @@
+function tickUp() {
+  const counter = document.getElementById("counter");
+  counter.textContent = counter.textContent +1;
+}
 
+function tickDown() {
+  const counter = document.getElementById("counter");
+  counter.textContent = counter.textContent+1;
+}
+
+function runForLoop() {
+  const counter = document.getElementById("counter").textContent;
+  let result = "";
+  for (let i = 0; i <= counter; i++){
+    result += i + " ";
+  }
+  document.getElementById("forLoopResult").textContent = result;
+}
+
+function showOddNumbers() {
+  const counter = document.getElementById("counter").textContent;
+  let result = "";
+  for (let i = 1; i <= counter; i++){
+    if ((i % 2) != 1){
+    result += i + " ";
+    }
+  }
+  document.getElementById("oddNumberResult").textContent = result;
+}
